903ef2317e104cbf1f6b29b6535cdaf37193473f
xt18393 <ytdxxt10@163.com>
2019-07-22 14:06:49 +0800

    add new file

---
+++ TEReplayTest/Classes/TETestViewController.h
@@ -0,0 +1,16 @@
+//
+//  TETestViewController.h
+//  Pods-TEReplayTest_Example
+//
+//  Created by offcn_Terry on 2019/7/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TETestViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TEReplayTest/Classes/TETestViewController.m
@@ -0,0 +1,31 @@
+//
+//  TETestViewController.m
+//  Pods-TEReplayTest_Example
+//
+//  Created by offcn_Terry on 2019/7/22.
+//
+
+#import "TETestViewController.h"
+
+@interface TETestViewController ()
+
+@end
+
+@implementation TETestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
